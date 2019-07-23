2996a404f64437a8ac04a532c8d1eadad246f95b
raeginez <rachelginez@yahoo.com>
2019-07-23 16:00:29 +1000

    added device, routes and top nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'homepage#home'
+
+  get 'items/boots', to: 'homepage#boots'
+  get 'items/flats', to: 'homepage#flats'
+  get 'items/heels', to: 'homepage#heels'
+  get 'items/trainers', to: 'homepage#trainers'
+
+                                                             
+
 end
