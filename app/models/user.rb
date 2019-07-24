9bd933071fe7acdaff809a8a90379c7d7cfa4f5b
raeginez <rachelginez@yahoo.com>
2019-07-24 12:02:57 +1000

    added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # def name
-  #   return username ? username :email
-  # end
+  def name
+    return username ? username : email
+  end
 end
