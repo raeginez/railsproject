797a4f5d2c66008d38df372dda636b371dd47636
lelani82 <lelaniparker@hotmail.com>
2019-07-29 12:15:53 +1000

    added stripe html

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,20 @@
+class OrdersController < ApplicationController
+  def new
+    @listing = Listing.find(params[:listing_id])
+
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :api_key)
+    @session = Stripe::Checkout::Session.create(
+    payment_method_types: ['card'],
+    line_items: [{
+    name: @listing.name,
+    # description: "by #{@book.author.name}",
+    images: ['https://example.com/t-shirt.png'],
+    amount: (@listing.price * 100).to_i,
+    currency: 'aud',
+    quantity: 1,
+    }],
+    success_url: 'http://localhost:3000/orders/complete',
+    cancel_url: 'http://localhost:3000/orders/cancel',
+    )
+  end
+end
