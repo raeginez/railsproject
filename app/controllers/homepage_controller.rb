94d727898ff516c076b386b37323fc60fc5c62bc
raeginez <rachelginez@yahoo.com>
2019-07-30 15:39:22 +1000

    displayed items in their categories

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,4 +2,19 @@ class HomepageController < ApplicationController
     def home
     end
 
+    def flats
+        @listing = Listing.where(category: "Flats")
+    end
+
+    def boots
+        @listing = Listing.where(category: "Boots")
+    end
+
+    def heels
+        @listing = Listing.where(category: "Heels")
+    end
+
+    def trainers
+        @listing = Listing.where(category: "Trainers")
+    end
 end
